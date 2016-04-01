313bd38c9c306a37fc03a5f41c32691e7ac176de
aida <onlost1@gmail.com>
2016-04-01 22:05:01 +0300

    SQL

--- task8.2.py
+++ task8.2.py
@@ -7,11 +7,11 @@ except:
 count = 0
 for line in fh:
     if line.startswith('From'):
-        line.rstrip() 
+        line.rstrip()
         words = line.split()
         if not words[0].startswith('From:'):
             email = words[1]
             print email
-            count = count + 1
+            count += 1
 
 print "There were", count, "lines in the file with From as the first word"
